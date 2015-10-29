a797b49d1859bfac7e1f99e8139f1a575c983c32
kamilkurop <kamilkurop@gmail.com>
2015-10-29 07:19:34 +0100

    Pętla for - rozmowa o możliwych przebiegach sterowania, w zależnosci od wartosci parametru miesiac.

---
+++ spotkanie_02/src/Dates.java
@@ -0,0 +1,30 @@
+
+/**
+ * Created by Kamil on 2015-10-22.
+ */
+public class Dates {
+
+    static int[] days = {31, 28, 31, 30};
+
+    public static void main(String[] args){
+        if (args.length==0){
+            System.out.println("Podaj miesi¹c i dzieñ");
+            return;
+        }
+        int dzien = Integer.parseInt(args[1]);
+        int miesiac = Integer.parseInt(args[0]);
+
+        System.out.println(
+                "Liczba dni od pocz¹tku roku: " + getTotalDays(miesiac, dzien)
+        ); //TDD
+    }
+
+    public static int getTotalDays(int miesiac, int dzien){
+
+        int suma = 0;
+        for (int a=1; a<miesiac; a++){
+            suma += days[a-1];
+        }
+        return suma + dzien;
+    }
+}
